899a6a9f4bdea180da10c003c75f4c099b8e32e2
Javier Luque Sáiz <167310505+JavierLuqueSaiz@users.noreply.github.com>
2024-05-29 11:20:26 +0200

    Update main_fine_tuning.py

--- TransformersTuning/main_fine_tuning.py
+++ TransformersTuning/main_fine_tuning.py
@@ -30,18 +30,18 @@ if __name__ == "__main__":
             "lang": "english",
             "model_name": "microsoft/deberta-base",
         },
-        # 2: {
-        #     "lang": "spanish",
-        #     "model_name": "dccuchile/bert-base-spanish-wwm-uncased",
-        # },
-        # 3: {
-        #     "lang": "spanish",
-        #     "model_name": "PlanTL-GOB-ES/roberta-base-bne",
-        # },
-        # 4: {
-        #     "lang": "spanish",
-        #     "model_name": "bert-base-multilingual-uncased"
-        # }
+         2: {
+             "lang": "spanish",
+             "model_name": "dccuchile/bert-base-spanish-wwm-uncased",
+         },
+         3: {
+             "lang": "spanish",
+             "model_name": "PlanTL-GOB-ES/roberta-base-bne",
+         },
+         4: {
+             "lang": "spanish",
+             "model_name": "bert-base-multilingual-uncased"
+         }
     }
     
     hyperparams = {
@@ -92,30 +92,55 @@ if __name__ == "__main__":
         #measure = "Yeshwant123/mcc"
         #measure = "mcc"
     
-        patience = 3 #[5, 10]
-        max_length = 128 #[This value can be estimated on the training set]
+        #patience = 3 #[5, 10]
+        #max_length = 128 #[This value can be estimated on the training set]
     
-    
+        Start a parent run for this preconfiguration
+        parent_run = wandb.init(project='LNR',
+                                entity='javier-luque',
+                                group=f'{lang}_{model_name}',
+                                job_type='model')
+        parent_run.config.update(preconfig)
+        parent_run.config.update({"SEED":SEED})
+        
+        
         print("Loading Tokenizer", model_name)
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         print("Loading Transformer Model", model_name)
         model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
-    
+
+        runs = 0
+        
         #Split training data in train and validation partition using hold out. It would be interesting use a K-Fold validation strategy.
-        X_train, X_val = train_test_split(X, test_size=0.1, random_state=SEED, shuffle=True, stratify=X['label'])
+        #X_train, X_val = train_test_split(X, test_size=0.1, random_state=SEED, shuffle=True, stratify=X['label'])
+
+        for fold, (train_index, val_index) in enumerate(kf.split(X)):
+            X_train, X_val = X.iloc[train_index], X.iloc[val_index]
+            with wandb.init(project=f'LNR_{formatted_datetime}',
+                            entity='javier-luque',
+                            group=f'{lang}_{model_name}',
+                            job_type=f'hyperparam-tuning-{run_counter}',
+                            name=f'{lang}_{model_name}_{run_counter}_fold_{fold}'
+                            ) as fold_run:
+                fold_run.config.update(preconfig)
+                fold_run.config.update(config)
+                fold_run.config.update({"SEED":SEED})
+
+                # Log the fold number
+                fold_run.config.update({"fold": fold + 1})
+            
+                #FINE-TUNNING the model and obtaining the best model across all epochs
+                fineTmodel=training(_wandb=fold_run, _model=model, _train_data=X_train, _val_data=X_val,_learning_rate=config["learning"],
+                                    _optimizer_name=config["optimizer_name"], _schedule=config["schedule"],  _epochs=config["epochs"], _tokenizer=config["tokenizer"], _batch_size=config["batch_size"],
+                                    _padding="max_length", _max_length=config["max_length"], _truncation=True, _patience=config["patience"], _measure=config["measure", _out="./out")
+            
+            
+                #VALIDATING OR PREDICTIONG on the test partition, this time I'm using the validation set, but you have to use the test set.
+                test_data=X_val
+                preds=validate(_wandb=fold_run, _model=fineTmodel, _test_data=X_val, _tokenizer=tokenizer, _batch_size=config["batch_size"], _padding="max_length", _max_length=config["max_length"], _truncation=True, _measure=config["measure"], evaltype=True)
     
-        #FINE-TUNNING the model and obtaining the best model across all epochs
-        fineTmodel=training(_model=model, _train_data=X_train, _val_data=X_val,_learning_rate= learning,
-                            _optimizer_name=optimizer_name, _schedule=schedule,  _epochs=epochs, _tokenizer=tokenizer, _batch_size=batch_size,
-                            _padding="max_length", _max_length=max_length, _truncation=True, _patience=patience, _measure= measure, _out="./out")
     
     
-        #VALIDATING OR PREDICTIONG on the test partition, this time I'm using the validation set, but you have to use the test set.
-        test_data=X_val
-        preds=validate(_model=fineTmodel, _test_data=X_val, _tokenizer=tokenizer, _batch_size=batch_size, _padding="max_length", _max_length=max_length, _truncation=True, _measure=measure, evaltype=True)
-
-
-
-
-
-
+    
+    
+    
